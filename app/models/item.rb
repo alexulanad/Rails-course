e332248a5ca7210ce269df5f95bfe3ee36abb48a
alexulanad <alexulanad@gmail.com>
2021-07-05 17:21:32 +0300

    Добавлена валидация для модели Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :price, numericality: { greater_than: -1 }
+  #validates :name, :description, presence: true
+
+  after_initialize  {p 'Инициализация'} # Item.new, Item.first, Item.last
+  after_save        {p 'Сохранено'}     # Item.save, Item.create
+  after_create      {p 'Создано'}       # Item.create
+  after_update      {p 'Обновлено'}     # Item.update
+  after_destroy    {p 'Удалено'}       # Item.last.destroy
 end
