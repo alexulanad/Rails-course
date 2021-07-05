2747f6720156e39018c951dc7a3b65966a44e16c
alexulanad <alexulanad@gmail.com>
2021-07-05 21:29:54 +0300

    Добавлен action create для контроллера items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,25 @@
 class ItemsController < ApplicationController
+  layout false
+  skip_before_action :verify_authenticity_token
+
   def index
     @items = Item.all
     #render body: @items.map {|item| "#{item.name}, #{item.price}, #{item.description}"}    
+  end
+
+  def create
+    item = Item.create(item_params)
+    #render json: item.name, status: :created
+    if item.persisted? # если item сохранен
+      render json: "Данные сохранены" # рендер в формате json
+    else # если item не прошел валидацию и не сохранился
+      render json: item.errors # рендер в формате json ошибки items
+    end    
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get '/index' => 'items#index'  
+  get '/index', to: 'items#index'
+
+  resources :items
 end
