f745341fda21f6e451d630723e3768de414742cb
alexulanad <alexulanad@gmail.com>
2021-07-06 18:40:58 +0300

    Добавлен метод show в контроллере items и вывод данных отдельной записи

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,15 +13,19 @@ class ItemsController < ApplicationController
 
   def create
     item = Item.create(item_params)
-    #redirect_to items_path
-    #render json: item.name, status: :created
     if item.persisted? # если item сохранен
-      render json: "Данные сохранены" # рендер в формате json
+      redirect_to items_path
+      #render json: item.name, status: :created
+      #render json: "Данные сохранены" # рендер в формате json
     else # если item не прошел валидацию и не сохранился
       render json: item.errors # рендер в формате json ошибки items
     end    
   end
 
+  def show
+    # Rails переходит на метод show, после того, как перехватывает из браузера маршрут вида /items/:id где :id - номер записи в базе данных, и сохраняет этот id в хеше params для дальнейшего использования
+    @item = Item.find(params[:id]) # Поиск записи в базе данных по id, переданного в хеше params. Сохраняет в переменной экземпляра массив с хешем данных найденного объекта
+  end
 
   private
 
