8bd806f67e8769761f5a2e11cdc6aee8d295295c
alexulanad <alexulanad@gmail.com>
2021-07-07 19:05:16 +0300

    Добавлено редактирование записей для Item, добавлено быстрое добавление тестовых записей в базу

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   #layout false
-  skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token
 
   def index
     @items = Item.all
@@ -12,25 +12,39 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.create(item_params)
-    if item.persisted? # если item сохранен
+    if params[:name] == "Запись"
+      item = Item.new(
+        name: params[:name], 
+        price: params[:price], 
+        weight: params[:weight], 
+        description: params[:description])
+      item.save
       redirect_to items_path
+    else
+      item = Item.create(item_params)
+      if item.persisted? # если item сохранен то
+        redirect_to items_path # перееаправить на страницу всех объектов Item
       #render json: item.name, status: :created
       #render json: "Данные сохранены" # рендер в формате json
-    else # если item не прошел валидацию и не сохранился
-      render json: item.errors # рендер в формате json ошибки items
+      else # если item не прошел валидацию и не сохранился
+        render json: item.errors # рендер в формате json ошибки items
+      end
     end    
   end
 
   def show
     # Rails переходит на метод show, после того, как перехватывает из браузера маршрут вида /items/:id где :id - номер записи в базе данных, и сохраняет этот id в хеше params для дальнейшего использования
-    @item = Item.find(params[:id]) # Поиск записи в базе данных по id, переданного в хеше params. Сохраняет в переменной экземпляра массив с хешем данных найденного объекта        
+    # Поиск записи в базе данных по id, переданного в хеше params. Сохраняет в переменной экземпляра массив с хешем данных найденного объекта        
+    @item = Item.find(params[:id])    
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
-  def update
+  def update    
+    Item.find(params[:id]).update(item_params)    
+    redirect_to item_path(params[:id])
   end
 
   def destroy    
